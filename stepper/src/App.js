ef1e2429f07ab65d1c61e2688565070a61a0955d
Tia dev <morganah724@gmail.com>
2023-08-07 16:26:45 +0330

    Adding more state to stepper project

--- stepper/src/App.js
+++ stepper/src/App.js
@@ -8,26 +8,34 @@ const messages = [
 
 function App() {
   const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
   const previousHandler = () => {
-    if (step > 1) setStep(step - 1);
+    if (step > 1) setStep((s) => s - 1);
   };
   const nextHandler = () => {
-    if (step < 3) setStep(step + 1);
+    if (step < 3) setStep((s) => s + 1);
   };
   return (
-    <div className="stepper">
-      <div className="numbers">
-        <div className={`step ${step >= 1 && "active"}`}>1</div>
-        <div className={`step ${step >= 2 && "active"}`}>2</div>
-        <div className={`step ${step >= 3 && "active"}`}>3</div>
-      </div>
-      <p className="message">{messages[step - 1]}</p>
-      <div className="btns">
-        <button onClick={previousHandler}>Previous</button>
-        <button onClick={nextHandler}>Next</button>
-      </div>
-    </div>
+    <>
+      <button className="close-btn" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="stepper">
+          <div className="numbers">
+            <div className={`step ${step >= 1 && "active"}`}>1</div>
+            <div className={`step ${step >= 2 && "active"}`}>2</div>
+            <div className={`step ${step >= 3 && "active"}`}>3</div>
+          </div>
+          <p className="message">{messages[step - 1]}</p>
+          <div className="btns">
+            <button onClick={previousHandler}>Previous</button>
+            <button onClick={nextHandler}>Next</button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
