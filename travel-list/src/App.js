53b76687818704f3aed5e7eb9282f86717bd2f74
Tia dev <morganah724@gmail.com>
2023-08-13 15:21:15 +0330

    Added more features to travel list and updated it

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -28,7 +28,7 @@ const App = () => {
         onDeleteItem={deleteItemHandle}
         onUpdateItem={updateItemPacked}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 };
@@ -102,10 +102,22 @@ const Item = ({ item, onDeleteItem, onUpdateItem }) => {
     </li>
   );
 };
-const Stats = () => {
+const Stats = ({ items }) => {
+  if (!items.length)
+    return (
+      <p className="stats">Start adding some items to your packing list 🚀</p>
+    );
+  const itemsNum = items.length;
+  const packedNum = items.filter((item) => item.packed).length;
+  const percentPacked = Math.round((packedNum / itemsNum) * 100);
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentPacked === 100
+          ? `You got everything, Ready to go ✈`
+          : `You have ${itemsNum} items on your list, and you already packed
+        ${packedNum} - (${percentPacked}%)`}
+      </em>
     </footer>
   );
 };
